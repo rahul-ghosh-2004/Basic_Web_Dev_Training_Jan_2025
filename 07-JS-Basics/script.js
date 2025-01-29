89166ea9814a0bad5fac64380011e40590be0351
rahul-ghosh-2004 <iamrahulghoshinfo@gmail.com>
2025-01-29 12:10:16 +0530

    Adding code files for javascript day 1

---
+++ 07-JS-Basics/script.js
@@ -0,0 +1,74 @@
+// console.log("Hello World")
+
+// alert, prompt
+
+// comments
+
+// let myName = 'Keshab'  // String
+// let age = 21  // Number  int
+// let bmi = 21.65   // Number  float
+// let gender = "MALE"   // String
+// let isLoggedIn = false  // Boolean --> true / false
+
+// console.log(typeof myName)
+// console.log(typeof age)
+// console.log(typeof bmi)
+// console.log(typeof gender)
+// console.log(typeof isLoggedIn)
+
+// {}  --> Object
+// Object
+let details = {
+    // key: value;
+    name: "Sudesh",
+    age: 20,
+    gender: "MALE",
+    isLoggedIn: true
+}   
+
+// console.log(details)
+// console.log(typeof details)  // Object
+
+let course = null
+// let marks
+
+// console.log(course)
+// console.log(marks)
+
+// array
+
+let marks = [ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 ]
+
+// console.log(typeof marks)  // type --> object
+
+let temp = [ 10, "TEMP", true, { name: "Keshab" }, [ 1, 2, 3, 4, 5 ], null, "Super Man", "Iron Man", "Dare Devil", "Punisher" ]
+
+
+// console.log(temp[temp.length - 1])
+// console.log(temp[0])
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// const value = 1 // constant variable ( app agar data ko ek bar assign kar doge to bapas change nehi kar pao ge )
+
+// let value2 = 20
+// value2 = 30
+
+// console.log(value)
+// console.log(value2)
+
+
+/////////////////////////////////////////////////////////////////////////////////
+
+/*
+
+1. alert --> displaying pop ups on the browser (only browser)
+2. prompt --> getting data from the user (only browser)
+
+3. Data Types --> number, string, null, undefined, object, array, boolean
+4. let vs const
+
+5. typeof, array indexing, length property
+
+*/
