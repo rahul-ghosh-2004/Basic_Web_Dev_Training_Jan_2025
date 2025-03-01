96e95bf1e123fd3fe691f5701be91e5f5b920e25
rahul-ghosh-2004 <iamrahulghoshinfo@gmail.com>
2025-03-01 20:45:04 +0530

    Adding code files for document object model day1 regarding basics of dom properties

---
+++ 14-practice/script.js
@@ -0,0 +1,45 @@
+// if else related questions
+// 1. Write an if-else statement that checks if a number is positive, negative, or zero.
+// const number = 0
+
+// if (number > 0) {
+//     console.log("Positive")
+// } else if (number < 0) {
+//     console.log("Negative")
+// } else{
+//     console.log("Zero")
+// }
+
+// number should be greater than 0
+// number should be less than 0
+// +1, +2, -1
+// -3, -2, -1, 0, +1, +2, +3
+
+
+// 2. Write a program that checks if a number is even or odd.
+// we need a number
+// const number = 21;
+
+// if (number % 2 == 0) {
+//     console.log("Even Number")
+// } else {
+//     console.log("Odd Number")
+// }
+// number % 2 === 0
+// number % 2 !== 0
+
+
+
+// loop related questions
+// 1. Write a for loop that prints numbers from 1 to 10.
+// for (let i = 1; i<+10; i++) {
+//     console.log(i)
+// }
+
+// 2. Write a while loop that prints numbers from 5 to 1.
+// let i = 5
+
+// while (i>=1) {
+//     console.log(i)
+//     i--
+// }
